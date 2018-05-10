20b6b38219675354b469696dd497d79bd227b8cf
sungbin <21700613@handong.edu>
2018-05-11 02:11:01 +0900

    01 var let comment

---
+++ varAndletCommend.playground/Contents.swift
@@ -0,0 +1,25 @@
+//: Playground - noun: a place where people can play
+
+//my first swift conding.
+
+//declare comment: var, let
+//var is nomal variable
+//let is like static variable
+
+//Swift support contraction
+//  var [variableName]:[type]
+//  =>var [variableName]
+
+//But, can not support automatically to convert variable type
+//always be carefull about it.
+
+import Cocoa
+
+let maxSpeed:Int = 200
+
+var currentSpeed:Int = 100
+
+//maxSpeed += 10   // ->is error
+vcurrentSpeed += 10 // ->is correct
+
+//currentSpeed = 101.1 //-> is error
