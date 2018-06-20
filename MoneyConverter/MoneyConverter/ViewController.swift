dc11fa90a8b2cb8204e5e66e8a6452ca5c717e9e
sungbin <21700613@handong.edu>
2018-06-20 22:05:33 +0900

    Start Making Money Converter

---
+++ MoneyConverter/MoneyConverter/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoneyConverter
+//
+//  Created by 임성빈 on 2018. 6. 20..
+//  Copyright © 2018년 임성빈. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
