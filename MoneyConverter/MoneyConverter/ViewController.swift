b50c9f4970c9f7ee187868d5bbf6b2124e0fb414
sungbin <21700613@handong.edu>
2018-06-21 10:18:03 +0900

    complete MoneyConverter

--- MoneyConverter/MoneyConverter/ViewController.swift
+++ MoneyConverter/MoneyConverter/ViewController.swift
@@ -14,12 +14,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var currencySegment: UISegmentedControl!
+    
+    @IBOutlet weak var sourceMoneyField: UITextField!
+    @IBOutlet weak var targetMoneyLabel: UILabel!
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func convertMoney(_ sender: Any) {
+        let ratio : Double
+        switch currencySegment.selectedSegmentIndex {
+        case 0:
+            ratio = 0.00085
+        case 1:
+            ratio = 1178.5
+        default:
+            ratio = 1.0
+        }
+        
+        let targetMoneyString:String
+        if let sourceMoney = Double(sourceMoneyField.text!) {
+            targetMoneyString = "\(sourceMoney * ratio)"
+        } else {
+            targetMoneyString = "Error"
+        }
+        
+        targetMoneyLabel.text = targetMoneyString
+    }
+    
 }
 
